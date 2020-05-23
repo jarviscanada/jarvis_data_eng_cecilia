ed276afcc4c0dbc5a081c442b97297331ce3327b
Ceciliaaayang <46852288+Ceciliaaayang@users.noreply.github.com>
2020-05-23 03:53:21 +0000

    Add files via upload

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLambda.java
@@ -0,0 +1,52 @@
+package ca.jrvs.apps.grep;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JavaGrepLambda extends JavaGrepImp{
+
+  public static void main(String[] args){
+    if (args.length != 3){
+      throw new IllegalArgumentException("USAGE: regex rootPath outFile");
+    }
+    JavaGrepLambda javaGrepLambda = new JavaGrepLambda();
+    javaGrepLambda.setRegex(args[0]);
+    javaGrepLambda.setRootPath(args[1]);
+    javaGrepLambda.setOutFile(args[2]);
+
+    try{
+      javaGrepLambda.process();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public List<File> listFiles(String rootDir) {
+    List<File> fileLambda = new ArrayList<>();
+    try{
+      fileLambda = Files.walk(Paths.get(rootDir)).map(Path::toFile).filter(File::isFile).collect(
+          Collectors.toList());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return fileLambda;
+  }
+
+  @Override
+  public List<String> readLines(File inputFile){
+    List<String> lineLambda = new ArrayList<>();
+    try{
+      lineLambda = Files.lines(inputFile.toPath()).collect(Collectors.toList());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return lineLambda;
+  }
+
+}
